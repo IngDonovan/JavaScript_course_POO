5ce1a9dfc0ee6ca9c94b69694b4a075a3909da2b
Donovan Rojas <rojasdrm03@gmail.com>
2023-04-11 09:52:07 -0500

    Class in JS

--- main.js
+++ main.js
@@ -50,3 +50,28 @@ const juanita = new Student(
 // Student {name: 'Juanita Alejadra', age: 15, cursosAprobados: Array(2)}
 //juanita es una instancia de nuestro prototipo Student
 //esconde en proto nuestro metodo
+
+//prototipos con la sintaxis de clases
+
+class Student2 {
+    //con el metodo constructor
+    constructor(name, age, cursosAprobados){
+        this.name = name;
+        this.age = age;
+        this.cursosAprobados = cursosAprobados;
+    }
+    //crear metodos
+    aprobarCurso(nuevoCursito){
+        this.cursosAprobados.push(nuevoCursito);
+    }
+}
+
+//nueva instancia de este prototipo
+const miguelito = new Student2(
+    "Miguel",
+    28,
+    [
+        "Curso Análisis de Negocios para Ciencia de Datos",
+        "Curso de Principios de visualización de Datos "
+    ],
+);
